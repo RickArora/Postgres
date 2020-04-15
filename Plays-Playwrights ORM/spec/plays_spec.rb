3c494f965d7f67fd6e9b9dc69227ab5dc8324c33
Ricky Arora <your_email_here>
2020-04-15 16:50:38 -0400

    more broken tests, yay

--- Plays-Playwrights ORM/spec/plays_spec.rb
+++ Plays-Playwrights ORM/spec/plays_spec.rb
@@ -2,9 +2,8 @@ require 'plays'
 
 describe Play do 
     describe 'find by titile' do
-        subject (:list_of_plays) {Play.new}
         it 'returns the Play matching the given title' do
-            expect(Play.find_by_title('All My Sons').year).to eq(1947)
+            expect(Play.new.find_by_title('All My Sons').year).to eq(1947)
         end
     end
 end
