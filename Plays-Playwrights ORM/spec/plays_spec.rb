3c553e347a4393106ea5b3d7deb162824f4b9264
Ricky Arora <your_email_here>
2020-04-15 16:59:47 -0400

    updated spec

--- Plays-Playwrights ORM/spec/plays_spec.rb
+++ Plays-Playwrights ORM/spec/plays_spec.rb
@@ -3,7 +3,7 @@ require 'plays'
 describe Play do 
     describe 'find by titile' do
         it 'returns the Play matching the given title' do
-            expect(Play.new.find_by_title('All My Sons').year).to eq(1947)
+            expect(Play.find_by_title('All My Sons').year).to eq(1947)
         end
     end
 end
