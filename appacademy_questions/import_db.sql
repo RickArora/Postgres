7f32cc7ed2ff124a52564656b5e2103232b7cf65
Ricky Arora <your_email_here>
2020-04-17 15:17:42 -0400

    SQL db up

--- appacademy_questions/import_db.sql
+++ appacademy_questions/import_db.sql
@@ -17,15 +17,14 @@ CREATE TABLE questions (
 CREATE TABLE question_follows (   
     id INTEGER NOT NULL PRIMARY KEY,
     author_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
     FOREIGN KEY (author_id) REFERENCES users(id),
-    questions_id INTEGER NOT NULL,
-    FOREIGN KEY (questions_id) REFERENCES questions(id)
+    FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
 CREATE TABLE replies (
     id INTEGER NOT NULL PRIMARY KEY,
     parent_reply INTEGER,
-    subject_question VARCHAR(255),
     body VARCHAR(255),
     author_id INTEGER,
     subject_question VARCHAR(255) NOT NULL,
@@ -38,5 +37,5 @@ CREATE TABLE question_likes (
     question_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (questions_id) REFERENCES questions(id)
+    FOREIGN KEY (question_id) REFERENCES questions(id)
 );
