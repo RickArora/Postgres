ae917dc3d2a37654033fed5ff9d2418cbf17b5d2
Ricky Arora <your_email_here>
2020-04-17 10:38:55 -0400

    need help on the SQL

--- appacademy_questions/import_db.sql
+++ appacademy_questions/import_db.sql
@@ -1,10 +1,28 @@
+RAGMA foreign_keys = ON;
+
 CREATE TABLE users (
     fname VARCHAR(255),
-    1name VARCHAR(255)
+    lname VARCHAR(255)
 );
 
 CREATE TABLE questions (
     title VARCHAR(255),
     body VARCHAR(255),
-    author VARCHAR(255)
+    author VARCHAR(255) FOREIGN KEY REFERENCES --(something)
+);
+
+CREATE TABLE question_follows (
+    fname VARCHAR(255),
+    1name VARCHAR(255),
+    title VARCHAR(255),
+    body VARCHAR(255),
+    author VARCHAR(255) FOREIGN KEY REFERENCES --(something)
+);
+
+CREATE TABLE replies (
+    -- to do
+);
+
+CREATE TABLE question_likes (
+    -- to do
 );
